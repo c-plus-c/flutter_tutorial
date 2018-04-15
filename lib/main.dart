cdf3bf7728c3c3c41354ec505eb9b63c562f53f6
Asato Imanishi <dtm.music.producer@gmail.com>
2018-04-15 19:41:33 +0900

    Added interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
       padding: const EdgeInsets.all(32.0),
       child: new Row(
         children: <Widget>[
-          new Expanded(child: new Column(
+          new Expanded(
+              child: new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               new Container(
@@ -28,33 +29,24 @@ class MyApp extends StatelessWidget {
               ),
             ],
           )),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          new Text("41"),
+          new FavoriteWidget(),
         ],
       ),
     );
 
-    Column buildButtonColumn(IconData icon, String label){
+    Column buildButtonColumn(IconData icon, String label) {
       Color color = Theme.of(context).primaryColor;
 
       return new Column(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new Icon(icon, color:color),
+          new Icon(icon, color: color),
           new Container(
             margin: const EdgeInsets.only(top: 8.0),
-            child: new Text(
-              label,
-              style: new TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: color
-              )
-            ),
+            child: new Text(label,
+                style: new TextStyle(
+                    fontSize: 12.0, fontWeight: FontWeight.w400, color: color)),
           )
         ],
       );
@@ -82,16 +74,13 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
     );
 
     return new MaterialApp(
-      title: "Flutter Demo",
-      theme: new ThemeData(
-          primarySwatch: Colors.blue
-    ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Top Lakes"),
-        ),
-        body: new ListView(
-          children: [
+        title: "Flutter Demo",
+        theme: new ThemeData(primarySwatch: Colors.blue),
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("Top Lakes"),
+          ),
+          body: new ListView(children: [
             new Image.asset(
               "images/lake.jpg",
               width: 600.0,
@@ -101,9 +90,53 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
             titleSection,
             buttonSection,
             textSection
-          ]
+          ]),
+        ));
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  State createState() => new _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(0.0),
+          child: new IconButton(
+              icon: _isFavorited
+                  ? new Icon(Icons.star)
+                  : new Icon(Icons.star_border),
+              color: Colors.red[500],
+              onPressed: _toggleFavorite),
         ),
-      )
+        new SizedBox(
+          width: 18.0,
+          child: new Container(
+            child: new Text("$_favoriteCount"),
+          ),
+        )
+      ],
     );
   }
 }
